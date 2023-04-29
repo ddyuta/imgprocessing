5bdecef4a5c539d320cc7cea6d03546e25154026
Yuta_Domen <yutamarodd0730@gmail.com>
2023-04-29 22:04:55 +0900

    renew

--- create_image.py
+++ create_image.py
@@ -3,15 +3,15 @@ import cv2
 
 img = np.zeros((400,400,3), np.uint8)
 img[:, :] = [255, 0, 0]
-cv2.imwrite('blueImage.jpg', img)
+cv2.imwrite('image/blueImage.jpg', img)
 cv2.imshow('img1', img)
 
 img[:, :] = [0, 255, 0]
-cv2.imwrite('greenImage.jpg', img)
+cv2.imwrite('image/greenImage.jpg', img)
 cv2.imshow('img2', img)
 
 img[:, :] = [0, 0, 255]
-cv2.imwrite('redImage.jpg', img)
+cv2.imwrite('image/redImage.jpg', img)
 cv2.imshow('img3', img)
 
 cv2.waitKey(0)
