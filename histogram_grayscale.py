4474e0afcba308f59639729901d905ec7dad831b
Yuta_Domen <yutamarodd0730@gmail.com>
2023-05-02 00:05:33 +0900

    new

--- histogram_grayscale.py
+++ histogram_grayscale.py
@@ -5,9 +5,6 @@ from matplotlib import pyplot as plt
 #グレースケールで画像を読み込む
 img = cv2.imread('image/Lenna.jpg', 0)
 
-#画像を表示するが、ヒストグラムあとに表示されてしまう
-cv2.imshow('img', img)
-
 #ヒストグラムを計算し、描画
 plt.hist(img.ravel(), 256,[0, 255])
 
@@ -18,6 +15,3 @@ plt.show()
 
 #最頻値の値を出力
 print(statistics.mode(img.ravel()))
-
-cv2.waitKey()
-cv2.destroyAllWindows()
