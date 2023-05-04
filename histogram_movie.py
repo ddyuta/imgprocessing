61f2f7903eec884147d61fca0f46f5926cdb8466
Yuta_Domen <yutamarodd0730@gmail.com>
2023-05-04 18:08:37 +0900

    映像とヒストグラムの同時に表示

--- histogram_movie.py
+++ histogram_movie.py
@@ -1,7 +1,16 @@
 import cv2
 from matplotlib import pyplot as plt
 
-#'e'を押し下した際にプログラムを終了する関数
+#動画の読み込み
+capture = cv2.VideoCapture('image/sample30fps.mp4')
+#BGRそれぞれでヒストグラムを作成するために、ループ用tupleを作成
+color = ('b', 'g', 'r')
+fig = plt.figure()
+
+if not capture.isOpened():
+    print('動画ファイルを開けませんでした')
+    exit()
+#’e’を押した時にプログラムを終了する関数
 def press(event):
     if event.key == 'e':
         capture.release()
@@ -9,32 +18,31 @@ def press(event):
         plt.close()
         exit()
 
-capture = cv2.VideoCapture('image/sample.mp4')
-
-#BGRそれぞれでヒストグラムを作成するために、ループ用tupleを作成
-color = ('b', 'g', 'r')
-
-fig = plt.figure()
-
 while True:
     ret, frame = capture.read()
-    cv2.imshow('frame', frame)# cv2.imshow('frame', frame)
     if ret:
-        #ヒストグラムの初期化
+        cv2.imshow('frame', frame)
+
         plt.clf()
         for i, col in enumerate(color):
             #ヒストグラムを計算
             histr = cv2.calcHist([frame], [i], None, [256], [0, 255])
             #ヒストグラムを描画
             plt.plot(histr, color=col)
-        #ｘ軸を0-255、ｙ軸を0-20000の範囲で表示
+
+        #x軸とy軸の範囲を指定
         plt.xlim([0, 255])
-        plt.ylim([0,20000])
+        plt.ylim([0, 20000])
+
         #ヒストグラムを描画
         plt.pause(0.01)
-        #動画を表示
-        cv2.imshow('frame', frame)
-        #press関数を使用し、'e'を押し下した際にプログラムを終了
+
+        #’ｑ’を押したときにbreak
+        #この書き込みがないと映像が表示されない
+        k = cv2.waitKey(1) & 0Xff
+        if k == ord('q'):
+            break
+        #press関数を使用し、’e’を押したときにプログラム終了
         fig.canvas.mpl_connect('key_press_event', press)
 
     else:
